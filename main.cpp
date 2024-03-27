479754ce9b3e75ce7a3420ae6ea6fe8658408634
glolichen <jlche18659@gmail.com>
2024-03-27 11:10:22 -0400

    make singleton logger

--- main.cpp
+++ main.cpp
@@ -20,49 +20,21 @@
 #include "search.h"
 #include "book.h"
 #include "timeman.h"
+#include "logger.h"
 
 #define LOG_DIR "/home/jayden/Desktop/Programs/potato-chess/logs/"
 
 const std::string START_POS = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-// https://stackoverflow.com/a/674120
-class OutputStream : public std::ostream {
-private:
-	bool is_logging;
-public:
-	OutputStream(std::string file) {
-		is_logging = true;
-		std::freopen(file.c_str(), "w", stderr);
-	}
-	OutputStream() {
-		is_logging = false;
-	}
-
-	// log output
-	template <class T>
-	OutputStream &operator<<(const T &v) {
-		std::cout << v;
-		if (is_logging)
-			std::cerr << time(0) << " O: " << v;
-		return *this;
-	}
-
-	// log input
-	void log_input(const std::string text) {
-		if (is_logging)
-			std::cerr << time(0) << " I: " << text << "\n";
-	}
-};
-
-OutputStream *outputter;
+logger::Logger *logg;
 
 int main() {
 	#ifdef LOG_DIR
 		std::stringstream log_file_dir;
 		log_file_dir << LOG_DIR << "log" << time(0) << ".txt";
-		outputter = new OutputStream(log_file_dir.str());
+		logg = logger::Logger::get_instance(log_file_dir.str());
 	#else
-		outputter = new OutputStream();
+		logg = logger::Logger::get_instance();
 	#endif
 
 	srand(time(0));
@@ -87,7 +59,7 @@ int main() {
 		std::string line;
 		getline(std::cin, line);
 
-		outputter->log_input(line);
+		logg->log_input(line);
 
 		std::stringstream ss(line);
 		
@@ -95,9 +67,9 @@ int main() {
 		ss >> token;
 
 		if (token == "isready")
-			*outputter << "readyok\n";
+			*logg << "readyok\n";
 		else if (token == "uci")
-			*outputter << "id name Potato Chess\nid author Jayden Li\noption name Move Overhead type spin default 10 min 0 max 5000\nuciok\n";
+			*logg << "id name Potato Chess\nid author Jayden Li\noption name Move Overhead type spin default 10 min 0 max 5000\nuciok\n";
 		else if (token == "position") {
 			totalHalfMoves = 0;
 			ss >> token;
@@ -145,9 +117,9 @@ int main() {
 					int depth;
 					ss >> depth;
 					perft::PerftResult result = perft::test(board, depth);
-					*outputter << "total nodes: " << result.totalNodes << "\n";
-					*outputter << "time: " << result.time << "ms\n";
-					*outputter << "nodes per second: " << (result.totalNodes / result.time * 1000) << "\n";
+					*logg << "total nodes: " << result.totalNodes << "\n";
+					*logg << "time: " << result.time << "ms\n";
+					*logg << "nodes per second: " << (result.totalNodes / result.time * 1000) << "\n";
 					goto skip;
 				}
 				else if (token == "depth")
@@ -175,15 +147,15 @@ int main() {
 				// *outputter << result.move << "\n";
 				if (result.move == -1) {
 					board = oldBoard;
-					*outputter << "bestmove " << move::to_string(lastResult.move);
-					*outputter << " ponder " << move::to_string(lastResult.ponder) << "\n";
+					*logg << "bestmove " << move::to_string(lastResult.move);
+					*logg << " ponder " << move::to_string(lastResult.ponder) << "\n";
 					goto skip;
 				}
 			}
 			else {
 				int bookMove = book::book_move(board);
 				if (bookMove != -1) {
-					*outputter << "bestmove " << move::to_string(bookMove) << "\n";
+					*logg << "bestmove " << move::to_string(bookMove) << "\n";
 					continue;
 				}
 				
@@ -210,11 +182,11 @@ int main() {
 
 			lastResult = result;
 
-			*outputter << "bestmove " << move::to_string(bestMove);
+			*logg << "bestmove " << move::to_string(bestMove);
 			if (ponderMove != -1 && ponderMove != 0)
-				*outputter << " ponder " << move::to_string(ponderMove) << "\n";
+				*logg << " ponder " << move::to_string(ponderMove) << "\n";
 			else
-				*outputter << "\n";
+				*logg << "\n";
 		}
 		else if (token == "setoption") {
 			std::string optionName;
@@ -237,13 +209,13 @@ int main() {
 		}
 		// polyglot (opening book system) key
 		else if (token == "pgkey")
-			*outputter << book::gen_polyglot_key(board) << "\n";
+			*logg << book::gen_polyglot_key(board) << "\n";
 		else if (token == "bookmove") {
 			int move = book::book_move(board);
 			if (move == -1)
-				*outputter << "no book move found for this position\n";
+				*logg << "no book move found for this position\n";
 			else
-				*outputter << move::to_string(move) << "\n";
+				*logg << move::to_string(move) << "\n";
 		}
 		else if (token == "print")
 			board.print();
